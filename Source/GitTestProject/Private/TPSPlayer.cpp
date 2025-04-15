de664be4e4d07a13e1ffb65d5e173fbd3a056bdc
jacobyi0909 <jacobyi0909@gmail.com>
2025-04-15 17:41:58 +0900

    20250415

--- Source/GitTestProject/Private/TPSPlayer.cpp
+++ Source/GitTestProject/Private/TPSPlayer.cpp
@@ -3,12 +3,13 @@
 
 #include "TPSPlayer.h"
 
+#include "MyDamageType.h"
+
 // Sets default values
 ATPSPlayer::ATPSPlayer()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
@@ -28,6 +29,20 @@ void ATPSPlayer::Tick(float DeltaTime)
 void ATPSPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+float ATPSPlayer::TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent,
+	class AController* EventInstigator, AActor* DamageCauser)
+{
+	
+	if (DamageEvent.DamageTypeClass == UMyDamageType::StaticClass())
+	{
+		
+	}
+
+	return Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+
 
 }
 
--- Source/GitTestProject/Public/TPSPlayer.h
+++ Source/GitTestProject/Public/TPSPlayer.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/DamageEvents.h"
 #include "GameFramework/Character.h"
 #include "TPSPlayer.generated.h"
 
@@ -26,4 +27,7 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
+
 };
+
